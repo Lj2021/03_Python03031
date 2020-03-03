a8271c3448f2d5f965035fdc7666e54687c59030
Lj2021 <ylrcky2021@gmail.com>
2020-03-03 23:11:32 +0800

    Initial commit

---
+++ xs01_列表基本使用.py
@@ -0,0 +1,8 @@
+name_list = ["zhangshan", "lisi", "wangwu"]
+# print(name_list[0])
+
+print(name_list.index("lisi"))
+name_list[1] = "李四"
+name_list.append("王小二")
+name_list.insert(1,"xiaoxiao")
+print(name_list)
